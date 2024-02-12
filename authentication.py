7c76a5f8bcb4f1ee0f3410d5f415337d7346fb32
Mehdi Farhadian <farhadianmehdi1@gmail.com>
2024-02-12 06:25:35 +0330

    :technologist: Cleaning authentication file
    Cleaning authentication file a little bit.

--- authentication.py
+++ authentication.py
@@ -1,11 +1,12 @@
 def isAdminUser(user_id):
     file = open("admins.txt", "r")
-    content = file.read()
+    content = [int(x.strip()) for x in file.readlines()]
     file.close()
-    if str(user_id) in content:
+    
+    if user_id in content:
         return True
     return False
 
 def add_admin_to_file(new_admin):
     file = open("admins.txt", "a")
-    file.write(f",{new_admin}")
+    file.write(f"{new_admin}\n")
